35cc6fcaea13e485c9945edab499c741b58ed28f
Naylachan <82255308+naylachan@users.noreply.github.com>
2022-03-01 07:06:52 +0800

    Add files via upload

---
+++ database/bot/helpen.js
@@ -0,0 +1,393 @@
+const helpen = (prefix, botname, ownername, codename, arts) => {
+	return `☞ MENU ${botname} ☜
+
+Hello brother ${codename}.👋
+here is the menu *${botname}*
+==================
+${arts}
+==================
+☞ *BAHASA* [ 2 ]
+⍟ *${prefix}setbahasa* id
+⍟ *${prefix}setlanguage* en
+
+☞ *INFO* [ 7 ]
+⍟ *${prefix}scbot*
+⍟ *${prefix}mygrub*
+⍟ *${prefix}sticker*
+⍟ *${prefix}owner*
+⍟ *${prefix}chat* <text>
+⍟ *${prefix}infoowner*
+⍟ *${prefix}infobot*
+
+☞ *OWNERMENU* [ 11 ]
+⍟ *${prefix}setmode* <public/self>
+⍟ *${prefix}return* <code>
+⍟ *${prefix}setppbot* <reply_img>
+⍟ *${prefix}join* <link>
+⍟ *${prefix}setprefix* <code>
+⍟ *${prefix}bcgc* <text>
+⍟ *${prefix}bc* <text>
+⍟ *${prefix}dellprem* <@tag>
+⍟ *${prefix}addprem* <@tag>
+⍟ *${prefix}ban* <@tag>
+⍟ *${prefix}unban* <@tag>
+
+☞ *GROUPMENU* [ 23 ]
+⍟ *${prefix}tagall* 
+⍟ *${prefix}tagall2*
+⍟ *${prefix}tagall3* 
+⍟ *${prefix}tagall4* 
+⍟ *${prefix}tagall5* 
+⍟ *${prefix}hidetag* <text>
+⍟ *${prefix}leave* 
+⍟ *${prefix}delete* <reply>
+⍟ *${prefix}sms* <text>
+⍟ *${prefix}kickrandom*
+⍟ *${prefix}tagtime* <text>|<time>
+⍟ *${prefix}linkgrub* 
+⍟ *${prefix}listadmin* 
+⍟ *${prefix}setdesc* <text>
+⍟ *${prefix}setname* <text>
+⍟ *${prefix}group* <buka/tutup>
+⍟ *${prefix}listonline* 
+⍟ *${prefix}add* <62xxx>
+⍟ *${prefix}kick* <@tag>
+⍟ *${prefix}antivirtex* <on/off>
+⍟ *${prefix}antilink* <on/off>
+⍟ *${prefix}antitag* <on/off>
+⍟ *${prefix}antitoxic* <on/off>
+
+☞ *X-RANDOM* [ 22 ]
+⍟ *${prefix}xsadboy* <@tag>
+⍟ *${prefix}xpakboy* <@tag>
+⍟ *${prefix}xbaik* <@tag>
+⍟ *${prefix}xjago* <@tag>
+⍟ *${prefix}xjelek* <@tag>
+⍟ *${prefix}xcantik* <@tag>
+⍟ *${prefix}xpinter* <@tag>
+⍟ *${prefix}xbeban* <@tag>
+⍟ *${prefix}xkontol* <@tag>
+⍟ *${prefix}xhebat* <@tag>
+⍟ *${prefix}xwibu* <@tag>
+⍟ *${prefix}xharam* <@tag>
+⍟ *${prefix}xbabi* <@tag>
+⍟ *${prefix}xbego* <@tag>
+⍟ *${prefix}xganteng* <@tag>
+⍟ *${prefix}xanjing* <@tag>
+⍟ *${prefix}xmonyet* <@tag>
+⍟ *${prefix}xsadgirl* <@tag>
+⍟ *${prefix}xpakgirl* <@tag>
+⍟ *${prefix}xjahat* <@tag>
+⍟ *${prefix}xnolep* <@tag>
+⍟ *${prefix}xgoblok* <@tag>
+
+☞ *TAG-RANDOM* [ 22 ]
+⍟ *${prefix}sadboy* 
+⍟ *${prefix}pakboy* 
+⍟ *${prefix}baik* 
+⍟ *${prefix}jago* 
+⍟ *${prefix}jelek* 
+⍟ *${prefix}cantik* 
+⍟ *${prefix}pinter* 
+⍟ *${prefix}beban* 
+⍟ *${prefix}kontol* 
+⍟ *${prefix}hebat* 
+⍟ *${prefix}wibu* 
+⍟ *${prefix}haram* 
+⍟ *${prefix}babi* 
+⍟ *${prefix}bego* 
+⍟ *${prefix}ganteng* 
+⍟ *${prefix}anjing* 
+⍟ *${prefix}monyet* 
+⍟ *${prefix}sadgirl* 
+⍟ *${prefix}pakgirl* 
+⍟ *${prefix}jahat* 
+⍟ *${prefix}nolep* 
+⍟ *${prefix}goblok* 
+
+☞ *V-RANDOM* [ 22 ]
+⍟ *${prefix}vsadboy* 
+⍟ *${prefix}vpakboy* 
+⍟ *${prefix}vbaik* 
+⍟ *${prefix}vjago* 
+⍟ *${prefix}vjelek* 
+⍟ *${prefix}vcantik* 
+⍟ *${prefix}vpinter* 
+⍟ *${prefix}vbeban* 
+⍟ *${prefix}vkontol* 
+⍟ *${prefix}vhebat* 
+⍟ *${prefix}vwibu* 
+⍟ *${prefix}vharam* 
+⍟ *${prefix}vbabi* 
+⍟ *${prefix}vbego* 
+⍟ *${prefix}vganteng* 
+⍟ *${prefix}vanjing* 
+⍟ *${prefix}vmonyet* 
+⍟ *${prefix}vsadgirl* 
+⍟ *${prefix}vpakgirl* 
+⍟ *${prefix}vjahat* 
+⍟ *${prefix}vnolep* 
+⍟ *${prefix}vgoblok* 
+
+☞ *S-RANDOM* [ 22 ]
+⍟ *${prefix}ssadboy* 
+⍟ *${prefix}spakboy* 
+⍟ *${prefix}sbaik* 
+⍟ *${prefix}sjago* 
+⍟ *${prefix}sjelek* 
+⍟ *${prefix}scantik* 
+⍟ *${prefix}spinter* 
+⍟ *${prefix}sbeban* 
+⍟ *${prefix}skontol* 
+⍟ *${prefix}shebat* 
+⍟ *${prefix}swibu* 
+⍟ *${prefix}sharam* 
+⍟ *${prefix}sbabi* 
+⍟ *${prefix}sbego* 
+⍟ *${prefix}sganteng* 
+⍟ *${prefix}sanjing* 
+⍟ *${prefix}smonyet* 
+⍟ *${prefix}ssadgirl* 
+⍟ *${prefix}spakgirl* 
+⍟ *${prefix}sjahat* 
+⍟ *${prefix}snolep* 
+⍟ *${prefix}sgoblok* 
+
+☞ *RANDOMCEK* [ 22 ]
+⍟ *${prefix}bapercek*
+⍟ *${prefix}sangecek*
+⍟ *${prefix}pakgirlcek*
+⍟ *${prefix}pakboycek*
+⍟ *${prefix}kontolcek*
+⍟ *${prefix}haramcek*
+⍟ *${prefix}anjingcek*
+⍟ *${prefix}jahatcek*
+⍟ *${prefix}baikcek*
+⍟ *${prefix}bebancek*
+⍟ *${prefix}babicek*
+⍟ *${prefix}nolepcek*
+⍟ *${prefix}jagocek*
+⍟ *${prefix}pintarcek*
+⍟ *${prefix}begocek*
+⍟ *${prefix}goblokcek*
+⍟ *${prefix}jelekcek*
+⍟ *${prefix}cantikcek*
+⍟ *${prefix}gantengcek*
+
+☞ *TAGRATE* [ 22 ]
+⍟ *${prefix}bapertag* <@tag>
+⍟ *${prefix}sangetag* <@tag>
+⍟ *${prefix}pakgirltag* <@tag>
+⍟ *${prefix}pakboytag* <@tag>
+⍟ *${prefix}kontoltag* <@tag>
+⍟ *${prefix}haramtag* <@tag>
+⍟ *${prefix}anjingtag* <@tag>
+⍟ *${prefix}jahattag* <@tag>
+⍟ *${prefix}baiktag* <@tag>
+⍟ *${prefix}bebantag* <@tag>
+⍟ *${prefix}babitag* <@tag>
+⍟ *${prefix}noleptag* <@tag>
+⍟ *${prefix}jagotag* <@tag>
+⍟ *${prefix}pintartag* <@tag>
+⍟ *${prefix}begotag* <@tag>
+⍟ *${prefix}gobloktag* <@tag>
+⍟ *${prefix}jelektag* <@tag>
+⍟ *${prefix}cantiktag* <@tag>
+⍟ *${prefix}gantengtag* <@tag>
+
+☞ *C-RANDOM* [ 22 ]
+⍟ *${prefix}csadboy* <@tag>
+⍟ *${prefix}cpakboy* <@tag>
+⍟ *${prefix}cbaik* <@tag>
+⍟ *${prefix}cjago* <@tag>
+⍟ *${prefix}cjelek* <@tag>
+⍟ *${prefix}ccantik* <@tag>
+⍟ *${prefix}cpinter* <@tag>
+⍟ *${prefix}cbeban* <@tag>
+⍟ *${prefix}ckontol* <@tag>
+⍟ *${prefix}chebat* <@tag>
+⍟ *${prefix}cwibu* <@tag>
+⍟ *${prefix}charam* <@tag>
+⍟ *${prefix}cbabi* <@tag>
+⍟ *${prefix}cbego* <@tag>
+⍟ *${prefix}cganteng* <@tag>
+⍟ *${prefix}canjing* <@tag>
+⍟ *${prefix}cmonyet* <@tag>
+⍟ *${prefix}csadgirl* <@tag>
+⍟ *${prefix}cpakgirl* <@tag>
+⍟ *${prefix}cjahat* <@tag>
+⍟ *${prefix}cnolep* <@tag>
+⍟ *${prefix}cgoblok* <@tag>
+
+☞ *WALLPAPER* [ 5 ]
+⍟ *${prefix}wallml*
+⍟ *${prefix}wallpubg*
+⍟ *${prefix}wallneon*
+⍟ *${prefix}wallcode*
+⍟ *${prefix}wallrandom*
+
+☞ *DOWNLOAD* [ 6 ]
+⍟ *${prefix}ytmp4* <link>
+⍟ *${prefix}ytmp3* <link>
+⍟ *${prefix}tiktokmp4* <link>
+⍟ *${prefix}playmp3* <link>
+⍟ *${prefix}playmp4* <link>
+⍟ *${prefix}tiktokmp3* <link>
+
+☞ *RANDOM* [ 18 ]
+⍟ *${prefix}artinama* <text>
+⍟ *${prefix}artimimpi* <text>
+⍟ *${prefix}resepmasakan* <text>
+⍟ *${prefix}katajago* <text>
+⍟ *${prefix}besarkecil* <text>
+⍟ *${prefix}jumlahhuruf* <text>
+⍟ *${prefix}jumlahangka* <text>
+⍟ *${prefix}infogempa* 
+⍟ *${prefix}balikangka* <text>
+⍟ *${prefix}wikipedia* <text>
+⍟ *${prefix}balikhuruf* <text>
+⍟ *${prefix}bilangangka* <text>
+⍟ *${prefix}holoh* <text>
+⍟ *${prefix}heleh* <text>
+⍟ *${prefix}huluh* <text>
+⍟ *${prefix}hilih* <text>
+⍟ *${prefix}halah* <text>
+⍟ *${prefix}kapital* <text>
+
+☞ *STICKNEWS* [ 12 ]
+⍟ *${prefix}sedih* <@tag>
+⍟ *${prefix}takut* <@tag>
+⍟ *${prefix}marah* <@tag>
+⍟ *${prefix}pukul* <@tag>
+⍟ *${prefix}peluk* <@tag>
+⍟ *${prefix}tampar* <@tag>
+⍟ *${prefix}nangis* <@tag>
+⍟ *${prefix}kaget* <@tag>
+⍟ *${prefix}diam* <@tag>
+⍟ *${prefix}ketawa* <@tag>
+⍟ *${prefix}jijik* <@tag>
+⍟ *${prefix}tendang* <@tag>
+
+☞ *ANIME* [ 24 ]
+⍟ *${prefix}gon*
+⍟ *${prefix}killua*
+⍟ *${prefix}kakashi*
+⍟ *${prefix}tsunade*
+⍟ *${prefix}orochimaru*
+⍟ *${prefix}mitsuki*
+⍟ *${prefix}sarada*
+⍟ *${prefix}boruto*
+⍟ *${prefix}sakura*
+⍟ *${prefix}sasuke*
+⍟ *${prefix}minato*
+⍟ *${prefix}naruto*
+⍟ *${prefix}copper*
+⍟ *${prefix}nami*
+⍟ *${prefix}ussop*
+⍟ *${prefix}sanji*
+⍟ *${prefix}luffy*
+⍟ *${prefix}zoro*
+⍟ *${prefix}senku*
+⍟ *${prefix}nezuko*
+⍟ *${prefix}tanjirou*
+⍟ *${prefix}natsu*
+⍟ *${prefix}sagiri*
+⍟ *${prefix}rimuru*
+
+☞ *SEARCH* [ 10 ]
+⍟ *${prefix}grubwa* <text>
+⍟ *${prefix}arena* <text>
+⍟ *${prefix}amazon* <text>
+⍟ *${prefix}shopee* <text>
+⍟ *${prefix}thelazy* <text>
+⍟ *${prefix}cersex* <text>
+⍟ *${prefix}ytsearch* <text>
+⍟ *${prefix}rexdl* <text>
+⍟ *${prefix}mod* <text>
+⍟ *${prefix}sfile* <text>
+
+☞ *NEWS* [ 4 ]
+⍟ *${prefix}berita*
+⍟ *${prefix}kompas*
+⍟ *${prefix}okezone*
+⍟ *${prefix}antara*
+
+☞ *INFORMATION* [ 5 ]
+⍟ *${prefix}jam*
+⍟ *${prefix}jamdunia*
+⍟ *${prefix}jadwalbola*
+⍟ *${prefix}infohoax*
+⍟ *${prefix}coronameninggal*
+
+☞ *INFORMATION* [ 4 ]
+⍟ *${prefix}hadist*
+⍟ *${prefix}quran*
+⍟ *${prefix}asmaulhusna*
+⍟ *${prefix}kisahnabi*
+
+☞ *GAME* [ 6 ]
+⍟ *${prefix}tebakgambar*
+⍟ *${prefix}tebaklirik*
+⍟ *${prefix}tebakkimia*
+⍟ *${prefix}tebakjenaka*
+⍟ *${prefix}tebakbendera*
+⍟ *${prefix}caklontong*
+
+☞ *NSFW* [ 19 ]
+⍟ *${prefix}yuri*
+⍟ *${prefix}thighs*
+⍟ *${prefix}pussy*
+⍟ *${prefix}panties*
+⍟ *${prefix}orgy*
+⍟ *${prefix}ass*
+⍟ *${prefix}ahegao*
+⍟ *${prefix}bdsm*
+⍟ *${prefix}blowjob*
+⍟ *${prefix}cuckold*
+⍟ *${prefix}ero*
+⍟ *${prefix}cum*
+⍟ *${prefix}femdom*
+⍟ *${prefix}foot*
+⍟ *${prefix}gangbang*
+⍟ *${prefix}glasses*
+⍟ *${prefix}hentai*
+⍟ *${prefix}jahy*
+⍟ *${prefix}masturbation*
+
+☞ *AUDIO* [ 20 ]
+⍟ Please select audio 1-20
+⍟ For example *${prefix}audio8*
+
+☞ *APPLY* [ 05 ]
+⍟ Wearing a mask
+⍟ Hand wash
+⍟ Keep your distance
+⍟ Stay away from the crowd
+⍟ Restrict mobility
+
+☞ *RULES* [ 5 ]
+⍟ This bot does not save media/photos you send
+⍟ Make the most of this bot
+⍟ Don,t spam features/command bots
+⍟ This bot is for entertainment purposes only, and *not to be eaten*
+⍟ This bot is just a bot, can,t talk/do excessive hall like a human
+
+☞ *HUKUMAN* [ 2 ]
+⍟ Owner berhak memberikan teguran hingga sanksi terhadap user yang melanggar peraturan di atas
+⍟ Melanggar peraturan di atas dapat mendapatkan sanksi seperti banned/block user
+
+☞ *PUNISHMENT* [ 2 ]
+⍟ Owner has the right to give warnings to sanctions against users who violate the rules above
+⍟ Violating the rules above can get sanctions such as banned/block user
+
+☞ *THX•TO* [ 6 ]
+⍟ *${ownername}*
+⍟ *NAYLA [ OTHER ]*
+⍟ *LORD RIYAN*
+⍟ *LOLKILLERS*
+⍟ *ADIWAJSHING*
+⍟ *WHATSAPP*`
+}
+
+exports.helpen = helpen
+// NOTE THX TO JGN DI HAPUS YAA BANH, HARGAI SAYA(RIMURUBOTZ)
